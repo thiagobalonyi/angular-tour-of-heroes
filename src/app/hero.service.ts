e9f2c27eb744c2867b6836814e4cde52a2076a49
Thiago Balonyi <thiagocandal@gmail.com>
2022-10-14 15:16:48 -0300

    add: navigation

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -4,16 +4,21 @@ import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 
-@Injectable({
-  providedIn: 'root',
-})
+@Injectable({ providedIn: 'root' })
   export class HeroService {
 
-  getHeroes(): Observable<Hero[]> {
-      const heroes = of (HEROES);
+    constructor(private messageService: MessageService) { }
+
+    getHeroes(): Observable<Hero[]> {
+      const heroes = of(HEROES);
       this.messageService.add('HeroService: fetched heroes');
       return heroes;
+    }
+    
+    getHero(id: number): Observable<Hero> {
+      const hero = HEROES.find(h => h.id === id)!;
+      this.messageService.add(`HeroService: fetched hero id=${id}`);
+      return of(hero);
   }
 
-  constructor(private messageService: MessageService) { }
 }
